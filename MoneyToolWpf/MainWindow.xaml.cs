e71528ec8a5031b6fe7a71b6b24c69ecfe5694f2
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2021-03-24 23:46:41 +0300

    add event EventHandler SummaChanged

--- MoneyToolWpf/MainWindow.xaml.cs
+++ MoneyToolWpf/MainWindow.xaml.cs
@@ -19,61 +19,47 @@ namespace MoneyToolWpf
             cbCurrency1.SelectedIndex = cbCurrency2.SelectedIndex = 2; // RUB
             label1.Content = label2.Content = labelCompare.Content = "";
 
-            cbCurrency1.SelectionChanged += cbCurrency_SelectionChanged; //new System.Windows.Controls.SelectionChangedEventHandler(this.cbCurrency1_SelectionChanged);
-            tbSumma1.TextChanged += tbSumma_TextChanged;
-            cbCurrency2.SelectionChanged += cbCurrency_SelectionChanged;
-            tbSumma2.TextChanged += tbSumma_TextChanged;
-            btnCompare.Click += btnCompare_Click;
+            cbCurrency1.SelectionChanged += (sender, args) => SummaChanged(sender, args);
+            tbSumma1.TextChanged += (sender, args) => SummaChanged(sender, args);
+            cbCurrency2.SelectionChanged += (sender, args) => SummaChanged(sender, args);
+            tbSumma2.TextChanged += (sender, args) => SummaChanged(sender, args);
+
+            SummaChanged += new EventHandler(MainWindow_SummaChanged);
         }
 
         decimal summa1 = 0.00M;
         decimal summa2 = 0.00M;
 
-        void summa1Changed()
+        event EventHandler SummaChanged;
+        
+        void MainWindow_SummaChanged(object sender, EventArgs e)
         {
-            try
+            if (sender is ComboBox && (sender as ComboBox).Name == "cbCurrency1"
+                || sender is TextBox && (sender as TextBox).Name == "tbSumma1")
             {
-                summa1 = Math.Round(Int32.Parse(tbSumma1.Text) * CurrencyExtension.CurrencyRateInRubles((Currency)cbCurrency1.SelectedIndex), 2);
+                try
+                {
+                    summa1 = Math.Round(Int32.Parse(tbSumma1.Text) * CurrencyExtension.CurrencyRateInRubles((Currency)cbCurrency1.SelectedIndex), 2);
+                }
+                catch
+                {
+                    summa1 = 0.00M;
+                }
+                label1.Content = $"{summa1} ₽";
             }
-            catch
+            else // if (sender is ComboBox && (sender as ComboBox).Name == "cbCurrency2" || sender is TextBox && (sender as TextBox).Name == "tbSumma2")
             {
-                summa1 = 0.00M;
+                try
+                {
+                    summa2 = Math.Round(Int32.Parse(tbSumma2.Text) * CurrencyExtension.CurrencyRateInRubles((Currency)cbCurrency2.SelectedIndex), 2);
+                }
+                catch
+                {
+                    summa2 = 0.00M;
+                }
+                label2.Content = $"{summa2} ₽";
             }
-            label1.Content = $"{summa1} ₽";
-            labelCompare.Content = "";
-        }
-        void summa2Changed()
-        {
-            try
-            {
-                summa2 = Math.Round(Int32.Parse(tbSumma2.Text) * CurrencyExtension.CurrencyRateInRubles((Currency)cbCurrency2.SelectedIndex), 2);
-            }
-            catch
-            {
-                summa2 = 0.00M;
-            }
-            label2.Content = $"{summa2} ₽";
-            labelCompare.Content = "";
-        }
 
-        private void tbSumma_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if ((sender as TextBox).Name == "tbSumma1")
-                summa1Changed();
-            else
-                summa2Changed();
-        }
-
-        private void cbCurrency_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if ((sender as ComboBox).Name == "cbCurrency1")
-                summa1Changed();
-            else
-                summa2Changed();
-        } 
-
-        private void btnCompare_Click(object sender, RoutedEventArgs e)
-        {
             labelCompare.Content =
                 summa1 < summa2 ? "<" :
                 summa1 > summa2 ? ">" :
